cf7f3400e958de6c08ac7a7c3314c2518d8479d9
krishkcompanysec <krishnaanilkuma007@gmail.com>
2021-01-03 16:09:50 +0530

    Updates

---
+++ precache-manifest.3a0da53d2cf612d800b366a23cb069f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react_deploy_trial_2_vercel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f74f5886c4ec04f8fc8a",
+    "url": "/react_deploy_trial_2_vercel/static/css/main.3e825baf.chunk.css"
+  },
+  {
+    "revision": "0bb0215b8534c1eaba7e",
+    "url": "/react_deploy_trial_2_vercel/static/js/runtime~main.f7a83242.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react_deploy_trial_2_vercel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1dde8fb3dc24a5a81ff4",
+    "url": "/react_deploy_trial_2_vercel/static/js/2.03ded776.chunk.js"
+  },
+  {
+    "revision": "f74f5886c4ec04f8fc8a",
+    "url": "/react_deploy_trial_2_vercel/static/js/main.796b57c3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react_deploy_trial_2_vercel/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react_deploy_trial_2_vercel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react_deploy_trial_2_vercel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "1dde8fb3dc24a5a81ff4",
+    "url": "/react_deploy_trial_2_vercel/static/css/2.fb886048.chunk.css"
+  },
+  {
+    "revision": "101bf3106681186fe975cb7ddc52cd8a",
+    "url": "/react_deploy_trial_2_vercel/index.html"
+  }
+];
